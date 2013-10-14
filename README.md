8ad77c78f67d3e8105ab5f80feb68366731b36cc
Julian Jewel <julian@julianjewel.com>
2013-10-14 07:56:42 -0700

    Initial Version of LENS

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-PIX - Patient Identifier Cross Reference
+LENS - Life Event Notification Service
 ============
 
-Simple RESTful Service for Patient Identifier Cross Reference.
+Life Event Notification Service (LENS) is the notification engine for Virtual Lifetime Electronic Record (VLER). A consumer can subscribe to data changes through the RESTful VLER Subscription Service. The subscribers would get notified in their mechanism of choice when data changes happen on the VLER record. 
 
--- bin/server.js
+++ bin/server.js
@@ -1,79 +1,136 @@
 /**
- * Entry point for the RESTFul PIX Service. 
+ * Entry point for the LENS Listener. 
  *
  * Created by: Julian Jewel
  *
  */
-var express = require('express')
 var config = require('config');
+var redis = require('redis');
 var _ = require('underscore');
+var S = require('string');
+var fs = require('fs');
+var jsonpath = require('JSONPath');
+var request = require('request');
+
 // Export config, so that it can be used anywhere
 module.exports.config = config;
-// Connect to Oracle Database for Cross Reference
-var oracle = require("oracle");
-// Connection String from property file
-var connectData = { "tns":config.db.connString, "user": config.db.username, "password": config.db.password };
-// Generic pool for database connection
-var generic_pool = require('generic-pool');
+var NodeJms= require('nodejms');
 
-var pool = generic_pool.Pool({
-    name: 'oracle',
-    max: config.db.pool,
-    create: function(callback) {
-		oracle.connect(connectData, function(err, connection) {
-			if(err) {
-				throw err;
-			}
-			callback(null, connection);
-		});
-    },
-    destroy: function(db) {
-        db.close();
-    },
-	// specifies how long a resource can stay idle in pool before being removed
-    idleTimeoutMillis : config.db.timeout,
-     // if true, logs via console.log - can also be a function
-    log : config.db.debug
-	
-});
+var HashMap = require('hashmap').HashMap;
+var jmsClients = new HashMap();
+var notificationConfigMap = new HashMap();
 
-// TODO: Implement Connection Pooling
-oracle.connect(connectData, function(err, connection) {
-	if(err) {
-		throw err;
+for (var i = 0; i < config.notification.length; i++) {
+	notificationConfigMap.set(config.notification[i].name, config.notification[i]);
+	// Set JMS Client separately for convenient access
+	var jmsClient = new NodeJms(__dirname + "/" + config.notification[i].endpoint.destination.jmsConnectLibrary, config.notification[i].endpoint.destination); 
+	jmsClients.set(config.notification[i].name, jmsClient);
+}
+var client = redis.createClient(config.listener.redis.port || '6379', config.listener.redis.host || 'localhost');
+// Do client.auth
+
+function processErrorAndRollback(err, evt) {
+	if(config.debug) {
+		console.log(err);
 	}
-	createApp(pool);
-});
-// Create Express App
-function createApp(pool) {
-    var app = express();
+	// Something goes wrong - push event back to queue
+	client.lpush(evt[0], evt[1]);
+	// Listen again
+	client.blpop(config.listener.channel, '0', callback);
+}
 
-    app.configure(function () {
-		// Log
-        app.use(express.logger());
-        app.use(app.router);
-		// Simple Access Control - TODO: Preferences & Authorizations
-		// TODO: Implement Security
-        if (config.accessControl) {
-            var accesscontrol = require('../lib/accesscontrol');
-            app.use(accesscontrol.handle);
-        }
-		// Only for development
+function callback(err, evt){
+	if(!_.isArray(evt)) {
+		throw new Error("Event must have some value");
+	}
+	var channel = evt[0];
+	var object = evt[1];
+	
+	if(err) {
+		processErrorAndRollback(err, evt);
+		return;
+	}	
+	if(object) {
+		
 		if(config.debug) {
-			app.use(express.errorHandler({ showStack: true, dumpExceptions: true }));
+			console.log("Evicted from Queue:" + channel + " Object: " + object);
 		}
-    });
+		
+		var jsonObj = JSON.parse(object);
+		var collection = jsonObj.ns;
+		var record = jsonObj.o;
+		
+		// Check global subscriptions (subscribed to all changes in a collection)
+		var globalSubscription = config.globalSubscriptions[collection];
+		// Get SSN
+		var ssn = jsonpath.eval(jsonObj.o, '$..nc:PersonSSNIdentification.nc:IdentificationID');
+		// Check whether the SSN has a subscription
+		// Query CRUD - http://localhost:3001/core/serviceTreatmentRecords.subscriptions?query={"subscription:Subscription.subscription:CommonData.nc:Person.nc:PersonSSNIdentification.nc:IdentificationID":"987654321"}
+		
+		request(config.ecrud.url + '/'
+			 + collection + '.subscriptions?query={"subscription:Subscription.subscription:CommonData.nc:Person.nc:PersonSSNIdentification.nc:IdentificationID":"'
+			 + ssn + '"}', function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var json = JSON.parse(body);
+				json = json ? json : {};
+				if (json.length > 0) {
+					// Get the Subscriber ID
+					for(var i=0; i<json.length; i++) {
+						var subData = json[i];
+						var subId = subData['subscription:Subscription']['subscription:CommonData']['vler:Subscription']['vler:Addressing']['vler:ReplyTo']['vler:SubscriberId'];
+						var jsonSubId = JSON.parse('[{"name":"' + subId + '"}]');
+						if(_.isUndefined(globalSubscription)) {
+							globalSubscription = jsonSubId;
+						} else {
+							globalSubscription[globalSubscription.length] = jsonSubId; 
+						}
+					}
+				}
+				
+				if (_.isUndefined(globalSubscription) || _.isEmpty(globalSubscription)) {
+					// No configuration
+					// Listen again
+					client.blpop(config.listener.channel, '0', callback);
+					return;
+				}
+				for (var i = 0; i < globalSubscription.length; i++) {
+					var notificationConfigName = globalSubscription[i].name;
+					var notificationConfig = notificationConfigMap.get(notificationConfigName);
+					var toSend = JSON.stringify(jsonObj.o);
+					if(S(notificationConfig.accept).startsWith("application/atom+xml")) {
+						// TODO: John May
+						//toSend = json2Atom(jsonObj.o);
+					}
+					if(S(notificationConfig.endpoint.type).startsWith('JMS')) {
+						var jmsClient = jmsClients.get(notificationConfig.name);
+						jmsClient.sendMessageAsync(toSend, "text", null, function (err) {
+							if(err) {
+								processErrorAndRollback(err, evt);
+								return;
+							}
+							if(config.debug) {
+								console.log('Sent Message to JMS Queue:' + notificationConfig.endpoint.destination.destinationJndiName + ' Message:' + toSend);
+							}
+							// Listen again
+							client.blpop(config.listener.channel, '0', callback);
+							return;
+						});
+					}
+					
+				}
+			} else {
+				processErrorAndRollback(err, evt);
+				return;
+			}
+		});		
+		
+	}
+}
+// Start the process
+client.blpop(config.listener.channel, '0', callback);
 
-	// Include Router
-	var router = require('../lib/router')(pool);
-
-	// Get the patient identifier cross-reference
-	app.get('/:assigningAuthority/:identifier', router.getCorrespondingIds);
-
-	// Listen
-    app.listen(config.server.port, config.server.server, function () {
-        console.log('PIX server listening on port ' + config.server.port);
-    });
+if(config.debug) {
+	console.log('Listening on Redis Channel-' + (config.listener.redis.host || 'localhost') + ':' + (config.listener.redis.port || '6379'));
 }
 
 // Default exception handler
@@ -87,7 +144,12 @@ process.on( 'SIGINT', function() {
 })
 // Default exception handler
 process.on('exit', function (err) {
-	pool.drain(function() {
-		pool.destroyAllNow();
-	});
+	if(!_.isUndefined(client)) {
+		// Cleanup
+	}
+	if(!_.isUndefined(jmsClients)) {
+		jmsClients.forEach(function(jmsClient, key) {
+			jmsClient.destroy();
+		});
+	}
 });
