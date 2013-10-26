8ba575f1e1105ed1f8ac1349cc3977e7bc909ea9
Julian Jewel <julian@julianjewel.com>
2013-10-26 16:57:35 -0700

    Redis message encryption

--- bin/server.js
+++ bin/server.js
@@ -15,7 +15,9 @@ var jsonpath = require('JSONPath');
 var request = require('request');
 var Log = require('vcommons').log;
 var logger = Log.getLogger('LENS', config.log);
+var JsonFormatter = require('vcommons').jsonFormatter;
 var HashMap = require('hashmap').HashMap;
+var CryptoJS = require("crypto-js");
 
 UTIL = {};
 UTIL.XML = require('vcommons').objTree;
@@ -48,148 +50,156 @@ client.auth(config.redis.auth, function (err) {
 	logger.info('Authenticated ' +  config.redis.host + ':' + config.redis.port);
 	// Start the process
 	logger.info('Redis Listening to ' + config.redis.channel);
-	logger.trace('Popping Data from ' + config.redis.channel + ' and calling callback');
-	client.blpop(config.redis.channel, '0', callback);
-
+	logger.trace('Popping Data from ' + config.redis.channel + ' into ' + config.redis.processingChannel + ' with timeout ' + config.redis.timeout);
+	client.brpoplpush(config.redis.channel, config.redis.processingChannel, config.redis.timeout, callback);
 	logger.info(('Listening on Redis Channel-' + (config.redis.host || 'localhost') + ':' + (config.redis.port || '6379')));
 
 });
 
-// Do client.auth
-
-function processErrorAndRollback(err, evt) {
-    logger.error('Error occured during processing of event:' + err);
-    // Something goes wrong - push event back to queue
-    client.lpush(evt[0], evt[1]);
-    // Listen again
-    client.blpop(config.redis.channel, '0', callback);
-    logger.error('Rolled back element to channel:' + config.redis.channel);
+// Ensure that it continues to listen on the Redis Channel
+function callback(err, evt) {
+	
+    if (err) {
+		// Stop execution on error.
+		logger.error("Received error", err);
+		throw err;
+    }
+	// Process & Continue
+	processNotification(err, evt, function(err, evt){
+		if(err) {
+			logger.error('Error occured, possible elements in processing queue ', evt);
+		} else {
+			logger.info('Removing from Redis Processing Channel-' + (config.redis.host || 'localhost') + ':' + (config.redis.port || '6379'), config.redis.processingChannel);
+			client.lrem(config.redis.processingChannel, 1, evt);
+		}
+		logger.trace('Popping Data from ' + config.redis.channel + ' into ' + config.redis.processingChannel + ' with timeout ' + config.redis.timeout);
+		client.brpoplpush(config.redis.channel, config.redis.processingChannel, config.redis.timeout, callback);
+	});
 }
 
-function callback(err, evt) {
+function processNotification(err, evt, callback) {
+	
+    var object;
+    logger.trace("Evicted from Queue:" + evt);
+	if(!_.isUndefined(config.redis.encryption) && config.redis.encryption.enabled) {
+		logger.trace("Decrypting Event..");
+		var decrypted = CryptoJS.AES.decrypt(evt, config.redis.encryption.passPhrase, { format: JsonFormatter });
+		logger.trace("Decrypted Event:" + " Event: " + decrypted);
+		object = decrypted.toString(CryptoJS.enc.Utf8)
+		logger.trace("Decrypted String:" + " Event: " + object);
+	}	else {
+		object = evt;
+	}
+	
+	if(_.isUndefined(object)) {
+		logger.error("Received empty object", object);
+		logger.error("Possible elements in processing queue", object);
+		callback(new Error("Empty Object received" + evt), evt);
+	}
+	
+	var jsonObj = JSON.parse(object);
+	logger.trace("Parsing Object:" + " JSON: " + jsonObj);
+	var collection = jsonObj.ns;
+	var record = jsonObj.o;
 
-    if (err) {
-		if(evt) {
-			if (!_.isArray(evt)) {
-				processErrorAndRollback(err, evt);
+	logger.trace("Checking if there is a global subscription for collection: ", collection);
+	// Check global subscriptions (subscribed to all changes in a collection)
+	var globalSubscription = config.globalSubscriptions[collection];
+	logger.trace("Found global subscription for collection: ", globalSubscription);
+	// Get SSN
+	var ssn = jsonpath.eval(jsonObj.o, '$..nc:PersonSSNIdentification.nc:IdentificationID');
+	// Check whether the SSN has a subscription
+	// Query CRUD - http://localhost:3001/core/serviceTreatmentRecords.subscriptions?query={"subscription:Subscription.subscription:CommonData.nc:Person.nc:PersonSSNIdentification.nc:IdentificationID":"987654321"}
+
+	var operation = retry.operation(config.ecrud.retry);
+	// Get subscriptions by SSN
+	operation.attempt(function (currentAttempt) {
+		logger.info('Attempt ' + currentAttempt + ':' + config.ecrud.url + '/'
+			 + collection + '.subscriptions?query={"subscription:Subscription.subscription:CommonData.nc:Person.nc:PersonSSNIdentification.nc:IdentificationID":"'
+			 + ssn + '"}');
+		request(config.ecrud.url + '/'
+			 + collection + '.subscriptions?query={"subscription:Subscription.subscription:CommonData.nc:Person.nc:PersonSSNIdentification.nc:IdentificationID":"'
+			 + ssn + '"}', function (error, response, body) {
+
+			if (operation.retry(error)) {
+				logger.error('Retry failed with error:', error, 'Attempt:', currentAttempt);
 				return;
 			}
-		} else {
-			// retry globally if there is no element
-			throw err;
-		}
-    }
 
-    var channel = evt[0];
-    var object = evt[1];
+			if (!error && response.statusCode == 200) {
+				var json = JSON.parse(body);
+				json = json ? json : {};
+				logger.trace('CRUD returned subscriptions:  ', json);
+				if (json.length > 0) {
+					// Get the Subscriber ID
+					for (var i = 0; i < json.length; i++) {
+						var subData = json[i];
+						var subId = subData['subscription:Subscription']['subscription:CommonData']['vler:Subscription']['vler:Addressing']['vler:ReplyTo']['vler:SubscriberId'];
+						var jsonSubId = JSON.parse('[{"name":"' + subId + '"}]');
 
+						logger.trace('Adding to list of subscribers', jsonSubId);
 
-    if (object) {
-        logger.trace("Evicted from Queue:" + channel + " Object: " + object);
-
-        var jsonObj = JSON.parse(object);
-        var collection = jsonObj.ns;
-        var record = jsonObj.o;
-
-        logger.trace("Checking if there is a global subscription for collection: ", collection);
-        // Check global subscriptions (subscribed to all changes in a collection)
-        var globalSubscription = config.globalSubscriptions[collection];
-        logger.trace("Found global subscription for collection: ", globalSubscription);
-        // Get SSN
-        var ssn = jsonpath.eval(jsonObj.o, '$..nc:PersonSSNIdentification.nc:IdentificationID');
-        // Check whether the SSN has a subscription
-        // Query CRUD - http://localhost:3001/core/serviceTreatmentRecords.subscriptions?query={"subscription:Subscription.subscription:CommonData.nc:Person.nc:PersonSSNIdentification.nc:IdentificationID":"987654321"}
-
-        var operation = retry.operation(config.ecrud.retry);
-		// Get subscriptions by SSN
-        operation.attempt(function (currentAttempt) {
-            logger.info('Attempt ' + currentAttempt + ':' + config.ecrud.url + '/'
-                 + collection + '.subscriptions?query={"subscription:Subscription.subscription:CommonData.nc:Person.nc:PersonSSNIdentification.nc:IdentificationID":"'
-                 + ssn + '"}');
-            request(config.ecrud.url + '/'
-                 + collection + '.subscriptions?query={"subscription:Subscription.subscription:CommonData.nc:Person.nc:PersonSSNIdentification.nc:IdentificationID":"'
-                 + ssn + '"}', function (error, response, body) {
-
-                if (operation.retry(error)) {
-                    logger.error('Retry failed with error:', error, 'Attempt:', currentAttempt);
-                    return;
-                }
-
-                if (!error && response.statusCode == 200) {
-                    var json = JSON.parse(body);
-                    json = json ? json : {};
-                    logger.trace('CRUD returned subscriptions:  ', json);
-                    if (json.length > 0) {
-                        // Get the Subscriber ID
-                        for (var i = 0; i < json.length; i++) {
-                            var subData = json[i];
-                            var subId = subData['subscription:Subscription']['subscription:CommonData']['vler:Subscription']['vler:Addressing']['vler:ReplyTo']['vler:SubscriberId'];
-                            var jsonSubId = JSON.parse('[{"name":"' + subId + '"}]');
-
-                            logger.trace('Adding to list of subscribers', jsonSubId);
-
-                            if (_.isUndefined(globalSubscription)) {
-                                globalSubscription = jsonSubId;
-                            } else {
-                                globalSubscription[globalSubscription.length] = jsonSubId;
-                            }
-                        }
-                    }
-                    logger.trace('The subscriptions for this message:', globalSubscription);
-                    if (_.isUndefined(globalSubscription) || _.isEmpty(globalSubscription)) {
-                        // No configuration
-                        // Listen again
-                        logger.trace('Ignoring message', evt[1]);
-                        logger.info('Retrieving next message from channel', config.redis.channel);
-                        client.blpop(config.redis.channel, '0', callback);
-                        return;
-                    }
-                    for (var i = 0; i < globalSubscription.length; i++) {
-                        var notificationConfigName = globalSubscription[i].name;
-                        var notificationConfig = notificationConfigMap.get(notificationConfigName);
-                        logger.trace('Finding notification configuration for ', notificationConfigName);
-                        if (_.isUndefined(notificationConfig)) {
-                            continue;
-                        }
-                        var toSend = JSON.stringify(jsonObj.o);
-                        if (S(notificationConfig.accept).startsWith("application/atom+xml")) {
-                            logger.trace('Trying to convert JSON Obj to NIEM', jsonObj.o);
-							try {
-								var jsonFeed = feedTransformer.niemDocJsObjToJsonFeed(jsonObj.o, jsonObj.o._id, jsonObj.o.uploadDate);
-								logger.trace('Converted JSON Feed', jsonFeed);
-								toSend = xotree.writeXML(jsonFeed);
-								logger.trace('Converted XML Feed', toSend);
-							} catch (err) {
-								logger.error('JSON could not converted to feed', err);
-								throw err;
-							}
-                        }
-
-						client.lpush(notificationConfig.channel, toSend);
-						if (!_.isUndefined(config.trackSubscriptions[collection])) {
-							logger.trace('Track notification for collection ', collection);
-							trackNotifications(collection, json, jsonObj, function (err) {
-								if (err) {
-									logger.error('Error occured during tracking of notification ', collection);
-									processErrorAndRollback(err, evt);
-									return;
-								}
-								logger.info('Retrieving next message from channel', config.redis.channel);
-								client.blpop(config.redis.channel, '0', callback);
-							});
+						if (_.isUndefined(globalSubscription)) {
+							globalSubscription = jsonSubId;
 						} else {
-							logger.info('No tracking for notification defined. Retrieving next message from channel', config.redis.channel);
-							client.blpop(config.redis.channel, '0', callback);
-							return;
+							globalSubscription[globalSubscription.length] = jsonSubId;
 						}
 					}
-                } else {
-                    processErrorAndRollback(err, evt);
-                    return;
-                }
-            });
-        });
-    }
+				}
+				logger.trace('The subscriptions for this message:', globalSubscription);
+				if (_.isUndefined(globalSubscription) || _.isEmpty(globalSubscription)) {
+					// No configuration
+					// Listen again
+					logger.trace('Ignoring message', evt);
+					logger.info('Retrieving next message from channel', config.redis.channel);
+					return callback(null, evt);
+				}
+				for (var i = 0; i < globalSubscription.length; i++) {
+					var notificationConfigName = globalSubscription[i].name;
+					var notificationConfig = notificationConfigMap.get(notificationConfigName);
+					logger.trace('Finding notification configuration for ', notificationConfigName);
+					if (_.isUndefined(notificationConfig)) {
+						continue;
+					}
+					var toSend = JSON.stringify(jsonObj.o);
+					if (S(notificationConfig.accept).startsWith("application/atom+xml")) {
+						logger.trace('Trying to convert JSON Obj to NIEM', jsonObj.o);
+						try {
+							var jsonFeed = feedTransformer.niemDocJsObjToJsonFeed(jsonObj.o, jsonObj.o._id, jsonObj.o.uploadDate);
+							logger.trace('Converted JSON Feed', jsonFeed);
+							toSend = xotree.writeXML(jsonFeed);
+							logger.trace('Converted XML Feed', toSend);
+						} catch (err) {
+							logger.error('JSON could not converted to feed', err);
+							throw err;
+						}
+					}
+					if(!_.isUndefined(notificationConfig.encryption) && notificationConfig.encryption.enabled) {
+						var encrypted = CryptoJS.AES.encrypt(toSend, notificationConfig.encryption.passPhrase, { format: JsonFormatter });
+						client.lpush(notificationConfig.channel, encrypted);
+					} else {
+						client.lpush(notificationConfig.channel, toSend);
+					}
+					if (!_.isUndefined(config.trackSubscriptions[collection])) {
+						logger.trace('Track notification for collection ', collection);
+						trackNotifications(collection, json, jsonObj, function (err) {
+							if (err) {
+								logger.error('Error occured during tracking of notification, possible elements in processing queue ', collection);
+							}
+							logger.info('Retrieving next message from channel', config.redis.channel);
+							return callback(null, evt);
+						});
+					} else {
+						logger.info('No tracking for notification defined. Retrieving next message from channel', config.redis.channel);
+						return callback(null, evt);
+					}
+				}
+			} else {
+				logger.error('Error occured during quering CRUD, possible elements in processing queue ', collection);
+				return callback(new Error('Error occured during quering CRUD, possible elements in processing queue ' + collection), evt);
+			}
+		});
+	});
 }
 
 function trackNotifications(collection, subscription, data, callback) {
@@ -245,8 +255,6 @@ function trackNotifications(collection, subscription, data, callback) {
 // Default exception handler
 process.on('uncaughtException', function (err) {
     logger.error('Caught exception: ' + err);
-	// Continue listening
-	client.blpop(config.redis.channel, '0', callback);
 });
 // Ctrl-C Shutdown
 process.on('SIGINT', function () {
